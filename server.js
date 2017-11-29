189ba81c09bccf9aa2f4d5292becc7b334d7f6e9
renliangbo <renna0508@126.com>
2017-11-29 13:19:41 +0800

    mend port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 app.use((req, res, next) => {
@@ -38,5 +40,6 @@ app.get('/about', (req, res) => {
 		res.render('about.hbs', {pageTitle: 'About page'});
 })
 
-app.listen(3000)
-console.log(2)
+app.listen(port, () => {
+		console.log(`Server is up on port ${port}`)
+})

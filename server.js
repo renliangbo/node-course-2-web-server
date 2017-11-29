3576879fc1d08b54e34a0a92763a9b4187ea5e6f
renliangbo <renna0508@126.com>
2017-11-29 13:49:10 +0800

    add projects page

--- server.js
+++ server.js
@@ -40,6 +40,10 @@ app.get('/about', (req, res) => {
 		res.render('about.hbs', {pageTitle: 'About page'});
 })
 
+app.get('/projects', (req, res) => {
+		res.render('projects.hbs', {pageTitle: 'Projects'})
+})
+
 app.listen(port, () => {
 		console.log(`Server is up on port ${port}`)
 })
